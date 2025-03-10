2f08035a7ec63a2fdd782ea4dc39ac09d33315e8
Katerina Matejovska <katerinamatejovska@email.cz>
2025-03-10 19:31:57 +0100

    cviceni1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const goodbye = (name) => {
+    const emClose = document.querySelector(".email__closing");
+    emClose.textContent = `S pozdravem ${name}`;
+};
+
+goodbye('Kateřina Matějovská');
+
+const fillSubject = (subject) => {
+    document.querySelector('.email__subject').textContent = subject;
+};
+
+fillSubject(`BlaBla`);
+
+const fillBody = (body, name) => {
+    const emBody = document.querySelector(".email__body");
+    emBody.innerHTML += body;
+    const emClose = document.querySelector(".email__closing");
+    emClose.textContent = goodbye(name);
+};
+
+fillBody(`Toto je tělo emailu.`,`Kateřina Matějovská`);
 
